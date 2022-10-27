c455c107164e676d727a6ffb658fd8c7b5ec7be2
Araceli Coreno <113930596+amcoreno@users.noreply.github.com>
2022-10-27 17:23:15 +0000

    functions problems not done

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # from methods_help import method_help
-from functions import function
+from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
 # from returnStuff import returnS
 
 # method_help()
 function()
+welcome("celi")
+added = add_numbers(45,5)
+print(added)
+final = num_dividedFrom_add_numbers(45, added)
+print(final)
 # returnS()
